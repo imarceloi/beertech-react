4cdab58d4ef224b9d447eec2d99318f5d79a10b3
marcelo <marcelo.nascimento@ab-inbev.com>
2020-10-17 11:43:31 -0300

    Add memo to increase performance on render components

--- backup/Aula9_memo/src/components/Twidders/Comments.tsx
+++ backup/Aula9_memo/src/components/Twidders/Comments.tsx
@@ -1,31 +1,30 @@
-import React from "react";
+import React, { memo } from "react";
 
 interface ICommentsProps {
-    messages: Message[];
+  messages: Message[];
 }
 
 export interface Message {
-    id: number;
-    name: string;
-    body: string;
+  id: number;
+  name: string;
+  body: string;
 }
 
 const style = {
-    marginLeft: "30px"
-}
+  marginLeft: "30px",
+};
 
 const Comments = ({ messages }: ICommentsProps) => {
-    return (
-        <div style={style} className="comments">
-        {messages.map(({ id, name, body }) => (
-            <div key={id}>
-                <h4>{name}</h4>
-                <p style={{ width: "400px" }}>{body}</p>
-            </div>
-            )
-        )}
+  return (
+    <div style={style} className="comments">
+      {messages.map(({ id, name, body }) => (
+        <div key={id}>
+          <h4>{name}</h4>
+          <p style={{ width: "400px" }}>{body}</p>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
-export default Comments;
+export default memo(Comments);
--- backup/Aula9_memo/src/components/Twidders/index.tsx
+++ backup/Aula9_memo/src/components/Twidders/index.tsx
@@ -1,4 +1,4 @@
-import React, { RefObject, SyntheticEvent, useEffect, useState } from "react";
+import React, { RefObject, SyntheticEvent, useEffect, useState, useMemo, useCallback } from "react";
 import Axios from "axios";
 import Comments, { Message } from "./Comments";
 import FormPostTwidder from "./FormPostTwidder";
@@ -10,23 +10,23 @@ interface TwidderState {
 }
 
 interface TwidderRemote {
-  data: TwidderState[]
+  data: TwidderState[];
 }
 
 interface CommentsRemote {
-  data: Message[]
+  data: Message[];
 }
 
 const style = {
   margin: "30px",
-  padding: "10px"
-}
+  padding: "10px",
+};
 
 const messageStyle = {
   marginBottom: "20px",
   padding: "20px",
-  boxShadow: "2px 2px 7px #DDD"
-}
+  boxShadow: "2px 2px 7px #DDD",
+};
 
 const Twidder = () => {
   const [twidders, setTwidders] = useState<TwidderState[]>([]);
@@ -34,18 +34,20 @@ const Twidder = () => {
   useEffect(() => {
     const loadMessages = async () => {
       const { data: messages } = await Axios.get<null, TwidderRemote>("https://jsonplaceholder.typicode.com/posts");
-      
+
       const newMessages = await messages.map(async (message) => {
-        const { data: remoteComments } = await Axios.get<null, CommentsRemote>(`https://jsonplaceholder.typicode.com/posts/${message.id}/comments`);
+        const { data: remoteComments } = await Axios.get<null, CommentsRemote>(
+          `https://jsonplaceholder.typicode.com/posts/${message.id}/comments`
+        );
         message.comments = remoteComments;
-        
+
         return message;
       });
-      
+
       Promise.all(newMessages).then((messages) => {
         setTwidders(messages);
       });
-    }
+    };
 
     loadMessages();
   }, []);
@@ -60,37 +62,40 @@ const Twidder = () => {
       {
         id: 400,
         title: "Meu twidd teste",
-        comments: []
+        comments: [],
       },
-      ...twidders
+      ...twidders,
     ]);
-  }
+  };
 
-  const handleChangePost = (id: number) => {
-    const [newTwidders, twidd3] = [[...twidders], twidders[id]];
+  const handleChangePost = useCallback(
+    () => (id: number) => {
+      const [newTwidders, twidd3] = [[...twidders], twidders[id]];
 
-    newTwidders[id].title = twidd3.title = "Novo titulo";
+      newTwidders[id].title = twidd3.title = "Novo titulo";
 
-    setTwidders(newTwidders);
-  }
+      setTwidders(newTwidders);
+    },
+    [twidders]
+  );
 
   return (
     <section style={{ fontFamily: "sans-serif", fontSize: "1.3rem" }}>
-      
       <div style={style}>
         <FormPostTwidder handleCreatePost={handleCreatePost} />
-        <button onClick={() => handleChangePost(0)}>Change Message 1</button>
-        {twidders.length > 0 ? twidders.map(({ id, title, comments }) => (
-          <section key={id} style={messageStyle}>
-            <h3 style={messageStyle}>{title}</h3>
-            <p>Comentários | total: {comments.length}</p>
-            <Comments messages={comments} />
-          </section>
-        )
-        ) : 'Carregando...'}
+        <button onClick={() => handleChangePost()}>Change Message 1</button>
+        {twidders.length > 0
+          ? twidders.map(({ id, title, comments }) => (
+              <section key={id} style={messageStyle}>
+                <h3 style={messageStyle}>{title}</h3>
+                <p>Comentários | total: {comments.length}</p>
+                <Comments messages={comments} />
+              </section>
+            ))
+          : "Carregando..."}
       </div>
     </section>
   );
-}
+};
 
 export default Twidder;
